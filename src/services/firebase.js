f697cd60447cd30c3cd81b0b442df7085713a463
Rituraj Codes <riturajcodes@gmail.com>
2026-04-21 01:21:04 +0530

    Env file added

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBPe_H_WCmiy0EvwdO7rtsbQyozHS3qwgY",
-  authDomain: "endtermtaxi.firebaseapp.com",
-  projectId: "endtermtaxi",
-  storageBucket: "endtermtaxi.firebasestorage.app",
-  messagingSenderId: "787817097912",
-  appId: "1:787817097912:web:ccdddfc5fd77acc674224d",
-  measurementId: "G-CZZVYC05J1"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
